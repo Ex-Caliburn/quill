bae27152a6fa741b58b18d2ba81c6513e9d5eb5d
lijiye <lijiye@jinddaka.com>
2018-04-08 19:00:26 +0800

    '1'

--- core/selection.js
+++ core/selection.js
@@ -271,28 +271,28 @@ class Selection {
     let selection = document.getSelection();
     if (selection == null) return;
     if (startNode != null) {
-      if (!this.hasFocus()) this.root.focus();
-      let native = (this.getNativeRange() || {}).native;
-      if (native == null || force ||
-          startNode !== native.startContainer ||
-          startOffset !== native.startOffset ||
-          endNode !== native.endContainer ||
-          endOffset !== native.endOffset) {
-
-        if (startNode.tagName == "BR") {
-          startOffset = [].indexOf.call(startNode.parentNode.childNodes, startNode);
-          startNode = startNode.parentNode;
-        }
-        if (endNode.tagName == "BR") {
-          endOffset = [].indexOf.call(endNode.parentNode.childNodes, endNode);
-          endNode = endNode.parentNode;
-        }
-        let range = document.createRange();
-        range.setStart(startNode, startOffset);
-        range.setEnd(endNode, endOffset);
-        selection.removeAllRanges();
-        selection.addRange(range);
-      }
+      // if (!this.hasFocus()) this.root.focus();
+      // let native = (this.getNativeRange() || {}).native;
+      // if (native == null || force ||
+      //     startNode !== native.startContainer ||
+      //     startOffset !== native.startOffset ||
+      //     endNode !== native.endContainer ||
+      //     endOffset !== native.endOffset) {
+      //
+      //   if (startNode.tagName == "BR") {
+      //     startOffset = [].indexOf.call(startNode.parentNode.childNodes, startNode);
+      //     startNode = startNode.parentNode;
+      //   }
+      //   if (endNode.tagName == "BR") {
+      //     endOffset = [].indexOf.call(endNode.parentNode.childNodes, endNode);
+      //     endNode = endNode.parentNode;
+      //   }
+      //   let range = document.createRange();
+      //   range.setStart(startNode, startOffset);
+      //   range.setEnd(endNode, endOffset);
+      //   selection.removeAllRanges();
+      //   selection.addRange(range);
+      // }
     } else {
       selection.removeAllRanges();
       this.root.blur();
